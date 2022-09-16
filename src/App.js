b7d4332dfc2d7c0473d39c3df0a298821d0d28b2
ViktoriiaProm <108242248+ViktoriiaProm@users.noreply.github.com>
2022-09-17 01:56:11 +0300

    Added from

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-/** @format */
-
+import React from "react";
 import "./App.css";
+import Weather from "./Weather";
+
+
 
 export default function App() {
   return (
     <div className='App'>
-      <div className="container">
-      <h1>Weather App</h1>
-      <footer>
-        This project was created by
-        {" "}
-        <a href='https://melodious-narwhal-23c1bb.netlify.app/' target='_blank'>
-          Viktoriia Prominska
-        </a>{" "}
-        and is{" "}
-        <a
-          href='https://github.com/ViktoriiaProm/westher-react-app-first'
-          target='_blank'>
-          open-source on GitHub
-        </a>
-      </footer>
-    </div>
+      <div className='container'>
+        <h1>Weather App</h1>
+        <Weather />
+        <footer>
+          This project was created by{" "}
+          <a
+            href='https://melodious-narwhal-23c1bb.netlify.app/'
+            target='_blank'>
+            Viktoriia Prominska
+          </a>{" "}
+          and is{" "}
+          <a
+            href='https://github.com/ViktoriiaProm/westher-react-app-first'
+            target='_blank'>
+            open-source on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
