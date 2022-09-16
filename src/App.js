b20b1e36a23a0ab868c905f3292ba34ecbf3e402
ViktoriiaProm <108242248+ViktoriiaProm@users.noreply.github.com>
2022-09-17 00:40:23 +0300

    Added the GitHub  and profile repo URLs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+/** @format */
 
-import './App.css';
+import "./App.css";
 
 export default function App() {
   return (
-    <div className="App">
-      Weather App
+    <div className='App'>
+      <h1>Weather App</h1>
+      <footer>
+        This project was created by
+        {" "}
+        <a href='https://melodious-narwhal-23c1bb.netlify.app/' target='_blank'>
+          Viktoriia Prominska
+        </a>{" "}
+        and is{" "}
+        <a
+          href='https://github.com/ViktoriiaProm/westher-react-app-first'
+          target='_blank'>
+          open-source on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
-
-
